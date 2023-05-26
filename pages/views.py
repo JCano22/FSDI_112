7c50f28bb876851cb750b5ca8d912e051821137d
Jorge Cano <coronero@gmail.com>
2023-05-25 20:00:10 -0700

    FSDI-112 first commit

---
+++ pages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = "pages/home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
+
+
+class MissionPageView(TemplateView):
+    template_name = "pages/mission.html"
+
+
+class CareersPageView(TemplateView):
+    template_name = "pages/careers.html"
